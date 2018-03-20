2876b79f0e8ee8dc714bbeec1b429d18d354bdf4
dudick123 <brian.dudick@outlook.com>
2018-03-20 19:15:00 -0400

    Update createpipeline.groovy

--- job-dsl/createpipeline.groovy
+++ job-dsl/createpipeline.groovy
@@ -1,1 +1,29 @@
+pipelineJob('Your App Pipeline') { 
 
+  def repo = 'https://github.com/dudick123/jenkins-dsl-job-1.git' 
+  def sshRepo = 'git@github.com:dudick123/jenkins-dsl-job-1.git' 
+
+  description("Your App Pipeline") 
+  keepDependencies(false) 
+
+  properties{ 
+
+    githubProjectUrl (repo) 
+    rebuild { 
+      autoRebuild(false) 
+    } 
+  } 
+
+  definition { 
+
+    cpsScm { 
+      scm { 
+        git { 
+          remote { url(sshRepo) } 
+          branches('master') 
+          scriptPath('Jenkinsfile') 
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
+        } 
+      } 
+    } 
+  }
