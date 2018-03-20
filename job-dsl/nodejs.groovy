f6f251a89883c38c61422295b1541a1e6bf3a7b3
dudick123 <brian.dudick@outlook.com>
2018-03-20 18:56:24 -0400

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('jenkins-dsl-job-1') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
